3e53e809ee88d017c88ee57d521db113a209fdcd
Ryan Contento <contento.rk@gmail.com>
2021-06-29 12:58:35 -0400

    working through faction button fucntions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //battle size and faction button selection
 
+
 var userRating
 
 //battle size button selection  
@@ -30,24 +31,76 @@ document.getElementById('button3').onclick = function () {
 //faction button selection 
 
 document.getElementById('button4').onclick = function () {
-    console.log(this.id + " was clicked")
-    userRating = 4;
-    document.getElementById('button4').className = 'selected';
-    document.getElementById('button5').className = '';
-    document.getElementById('button6').className = '';
-}
-document.getElementById('button5').onclick = function () {
-    console.log(this.id + " was clicked")
-    userRating = 5;
-    document.getElementById('button4').className = '';
-    document.getElementById('button5').className = 'selected';
-    document.getElementById('button6').className = '';
-}
-document.getElementById('button6').onclick = function () {
-    console.log(this.id + " was clicked")
-    userRating = 6;
-    document.getElementById('button4').className = '';
-    document.getElementById('button5').className = '';
-    document.getElementById('button6').className = 'selected';
+      console.log(this.id + " was clicked")
+      userRating = 4;
+      document.getElementById('button4').className = 'selected';
+      document.getElementById('button5').className = '';
+      document.getElementById('button6').className = '';
+  }
+//FIX ME   function changeImperium() {
+    var x = document.getElementById('imperium');
+    if (x.style.display === 'none') {
+      x.style.display = 'block';
+    } else {
+      x.style.display = 'none';
+    }
+  }
+
+  document.getElementById('button5').onclick = function () {
+      console.log(this.id + " was clicked")
+      userRating = 5;
+      document.getElementById('button4').className = '';
+      document.getElementById('button5').className = 'selected';
+      document.getElementById('button6').className = '';
+  }
+  document.getElementById('button6').onclick = function () {
+      console.log(this.id + " was clicked")
+      userRating = 6;
+      document.getElementById('button4').className = '';
+      document.getElementById('button5').className = '';
+      document.getElementById('button6').className = 'selected';
+  }
+
+
+//making faction selection change which sub-faction options are available 
+
+var theParent = document.querySelector("#faction");
+theParent.addEventListener("click", doSomething, false);
+
+function doSomething(e) {
+  if (e.target !== e.currentTarget) {
+    var clickedItem = e.target.id;
+    alert("hello " + clickedItem);
+  }
+
+
 }
 
+//change which dropdown is visable based on which faction is selected 
+
+function changeImperium() {
+  var x = document.getElementById('imperium');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+function changeChaos() {
+  var x = document.getElementById('chaos');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+function changeXenos() {
+  var x = document.getElementById('xenos');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
