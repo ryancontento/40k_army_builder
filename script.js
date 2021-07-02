d5c639f5efec1295438214eaf2ce173c1318ddcc
Ryan Contento <contento.rk@gmail.com>
2021-07-02 11:49:43 -0400

    basic login added

--- script.js
+++ script.js
@@ -104,3 +104,23 @@ function addDedicatedTransportUnit() {
   entry.appendChild(document.createTextNode(dedicatedTransportUnit));
   list7.appendChild(entry);
 }
+
+//login v1
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "username" && password === "password") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
